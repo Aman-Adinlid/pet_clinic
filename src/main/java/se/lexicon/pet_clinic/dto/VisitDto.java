326a10df93e62828c1fb97a1c2925d0f0edc2ef8
aman adinlid <95pinkpanda@gmail.com>
2021-04-28 12:07:16 +0200

    ##Implmented visit dto##

--- src/main/java/se/lexicon/pet_clinic/dto/VisitDto.java
+++ src/main/java/se/lexicon/pet_clinic/dto/VisitDto.java
@@ -1,5 +1,13 @@
 package se.lexicon.pet_clinic.dto;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class VisitDto {
-    // todo: implement VisitDao
+    private String id;
+    private PetDto petDto;
+    private LocalDate visitDate;
+    private String description;
 }
