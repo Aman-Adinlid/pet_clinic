e6bd4f77ea217b79e13e60886dbb24133970a1d0
aman adinlid <95pinkpanda@gmail.com>
2021-04-28 11:16:47 +0200

    ##Implmented pet type repository##

--- src/main/java/se/lexicon/pet_clinic/repository/PetTypeRepository.java
+++ src/main/java/se/lexicon/pet_clinic/repository/PetTypeRepository.java
@@ -1,6 +1,12 @@
 package se.lexicon.pet_clinic.repository;
 
-public interface PetTypeRepository {
-    // todo: implement basic CRUD
+import org.springframework.data.repository.CrudRepository;
+import se.lexicon.pet_clinic.entity.PetType;
 
+import java.util.List;
+
+public interface PetTypeRepository extends CrudRepository<PetType, Integer> {
+    List<PetType> findById(int id);
+
+    List<PetType> findByName(String name);
 }
