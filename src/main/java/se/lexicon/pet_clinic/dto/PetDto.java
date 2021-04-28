45e2b719f4f84975d42b30977ba7c13a42bcec10
aman adinlid <95pinkpanda@gmail.com>
2021-04-28 10:15:09 +0200

    ##Implmented pet dto class##

--- src/main/java/se/lexicon/pet_clinic/dto/PetDto.java
+++ src/main/java/se/lexicon/pet_clinic/dto/PetDto.java
@@ -1,5 +1,24 @@
 package se.lexicon.pet_clinic.dto;
 
+import lombok.Data;
+import se.lexicon.pet_clinic.entity.Owner;
+import se.lexicon.pet_clinic.entity.PetType;
+
+import javax.persistence.CascadeType;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.time.LocalTime;
+
+@Data
 public class PetDto {
-    // todo: implement PetDto
+    private String id;
+    private String name;
+    private LocalTime birthDate;
+    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.REFRESH, CascadeType.DETACH})
+    @JoinColumn(name = "pet_type_id")
+    private PetType petType;
+    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.REFRESH, CascadeType.DETACH})
+    @JoinColumn(name = "owner_id")
+    private Owner owner;
 }
