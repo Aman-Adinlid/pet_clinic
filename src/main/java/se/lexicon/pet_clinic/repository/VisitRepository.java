ecc147d0ba118a18bcb02f1d7f3b39916fff21f7
aman adinlid <95pinkpanda@gmail.com>
2021-04-28 12:16:39 +0200

    ##Implmented visit repository##

--- src/main/java/se/lexicon/pet_clinic/repository/VisitRepository.java
+++ src/main/java/se/lexicon/pet_clinic/repository/VisitRepository.java
@@ -1,8 +1,20 @@
 package se.lexicon.pet_clinic.repository;
 
-public interface VisitRepository {
-    // todo: implement basic CRUD
+import org.springframework.data.repository.CrudRepository;
+import se.lexicon.pet_clinic.entity.Visit;
 
-    //todo: select visit by pet name
-    //todo: select visit by pet type name
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface VisitRepository extends CrudRepository<Visit, String> {
+    Optional<Visit> findById(String id);
+
+    List<Visit> findByVisitDate(LocalDate visitDate);
+
+    List<Visit> findByDescription(String description);
+
+    List<Visit> findVisitByPetName(String petName);
+
+    List<Visit> findVisitByPetPetTypeName(String petTypeName);
 }
